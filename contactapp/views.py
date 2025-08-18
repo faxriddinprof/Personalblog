29a4faa661499764fcc66d9d4ac74bf9be28627e
Faxriddin Baxtiyorov <faxriddinbaxtiyorov144@gmail.com>
2025-08-18 16:59:40 +0500

    contact page fixed

--- contactapp/views.py
+++ contactapp/views.py
@@ -6,5 +6,11 @@ from .forms import ContactForm
 def SendMessage(request):
     contact_form = ContactForm()
     template_name='contactapp/contact.html'
+    if request.method == 'POST':
+        contact_form = ContactForm(request.POST)
+        if contact_form.is_valid():
+            contact_form.save()
+            contact_form = ContactForm()
+
     context = {'contact_form':contact_form}
     return render(request, template_name=template_name, context=context)
